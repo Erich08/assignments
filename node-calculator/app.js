5568e7cc669ea7bd108371f5e75b0094d3f44d86
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-22 13:02:32 -0600

    Installed readline-sync and completed Node Calculator assignment

--- node-calculator/app.js
+++ node-calculator/app.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const readLine = require('readline-sync');
+
+const num1 = parseInt(readLine.question('Please enter your first number: '));
+const num2 = parseInt(readLine.question('Please enter your second number: '));
+let operation = readLine.question(
+  'Please enter the operation to perform: add, sub, mul, div: '
+);
+
+if (operation === 'add') {
+  console.log(add(num1, num2));
+} else if (operation === 'mul') {
+  console.log(mul(num1, num2));
+} else if (operation === 'div') {
+  console.log(div(num1, num2));
+} else if (operation === 'sub') {
+  console.log(sub(num1, num2));
+}
+
+function add() {
+  return num1 + num2;
+}
+
+function mul() {
+  return num1 * num2;
+}
+
+function div() {
+  return num1 / num2;
+}
+
+function sub() {
+  return num1 - num2;
+}
