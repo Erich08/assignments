f584632a2313efd32b145dfefa99ad8be119628a
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-23 10:29:10 -0600

    Completed all tasks except stretch goal

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const input = document.getElementById('input');
 const header = document.getElementById('header');
 const messages = document.querySelector('.messages');
+const theme = document.getElementById('theme-drop-down');
+const left = document.getElementsByClassName('left');
+const right = document.getElementsByClassName('right');
+const leftOne = document.querySelector('.messages :nth-child(1)');
+const rightOne = document.querySelector('.messages :nth-child(2)');
+const leftTwo = document.querySelector('.messages :nth-child(3)');
+const rightTwo = document.querySelector('.messages :nth-child(4)');
+
 
 // Created this div and added the class name header to center the h1 and p elements
 let headerDiv = document.createElement('div');
@@ -25,5 +34,39 @@ let clear = document.getElementById('clear-button').addEventListener('click', (e
     console.log('hello');
 })
 
-//TODO: Bronze - There's some sub par conversation going on in this messaging app. You have two tasks to help give it either a clean slate, or a positive start.
-// - Write some custom JavaScript to automatically change the words of the conversation to something fun and good.
+//My solution to target the specific conversations without changing each conversation to the same thing if I were to use getElementsByClassName for example. I used :nth-child(n) to accomplish this. Although, it is quite verbose with all of the variables I had to declare. I am wondering if I could have used a for loop instead, but I am not sure as to how I would be able to specify exactly where I want each sentence to be. 
+leftOne.textContent = 'I am learning JavaScript.';
+leftTwo.textContent = 'That is the plan.';
+rightOne.textContent = 'That is awesome! One day you will make a lot of money.';
+rightTwo.textContent = 'You will get there. You just have to be persistent.';
+
+theme.addEventListener('change', (e) => {
+    if(e.target.value === 'theme-one') {
+        leftOne.style.backgroundColor = 'blue';
+        rightOne.style.backgroundColor = 'brown';
+        leftTwo.style.backgroundColor = 'blue';
+        rightTwo.style.backgroundColor = 'brown';
+    } else if (e.target.value === 'theme-two') {
+        leftOne.style.backgroundColor = 'red';
+        rightOne.style.backgroundColor = 'black';
+        leftTwo.style.backgroundColor = 'red';
+        rightTwo.style.backgroundColor = 'black';
+        rightOne.style.color = 'white';
+        rightTwo.style.color = 'white';
+    } else if (e.target.value === 'none') {
+        leftOne.style.backgroundColor = '';
+        rightOne.style.backgroundColor = '';
+        leftTwo.style.backgroundColor = '';
+        rightTwo.style.backgroundColor = '';
+        rightOne.style.color = '';
+        rightTwo.style.color = '';
+    }
+})
+
+const newMessage = document.getElementById('new-message').addEventListener('click', (e) => {
+    e.preventDefault()
+    messages.append(input.value);
+    input.value = '';
+})
+
+
