d5971a52d19081eed6c04707e78144b3f5604422
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-23 13:42:09 -0600

    Completed DOM debug exercise

--- exercises/dom-debug/index.js
+++ exercises/dom-debug/index.js
@@ -1,28 +1,34 @@
 const colors = ["red", "blue", "green"]
 
-document.getElementById("add").addEventListener("onclick", function(e){
+
+//Changed event listener to listen for click instead of onclick.. Seems to be working now.
+document.getElementById("add").addEventListener("click", function(e){
     const subItem = createSubItem(e)
     document.getElementById("list").appendChild(subItem)
 })
 
+
+//Added .length so the for loop will iterate through the array. Added document to createElement on line 16. Tricky.. Tricky..
 function createDropDown(){
     const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
         option.innerHTML = colors[i]
         option.value = colors[i]
         dropDown.append(option)
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    //On line 23 I had to change parent to parentElement and also add .style to change the background color
+    dropDown.addEventListener("change", function(e){
+        e.target.parentElement.style.backgroundColor = e.target.value
     })
     return dropDown
 }
 
+//Added .value to line 28. It didn't know which value to add.
 function createSubItem(e){
     const subItem = document.createElement("div")
     var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
+    subItem.textContent = subItemValue.value
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
     subItem.setAttribute("class", "subItem")
