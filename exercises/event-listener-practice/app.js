68d41b929d7843a143d0af2964b8f43f1369e223
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-22 21:05:01 -0600

    added event listeners to satisfy the exercise requirements

--- exercises/event-listener-practice/app.js
+++ exercises/event-listener-practice/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const square = document.getElementById('square');
+
+square.addEventListener('mouseover', (e) => {
+  square.style.backgroundColor = 'blue';
+});
+
+square.addEventListener('mousedown', (e) => {
+  square.style.backgroundColor = 'red';
+});
+
+square.addEventListener('mouseup', (e) => {
+  square.style.backgroundColor = 'yellow';
+});
+
+square.addEventListener('dblclick', (e) => {
+  square.style.backgroundColor = 'green';
+});
+
+document.body.addEventListener('wheel', (e) => {
+  square.style.backgroundColor = 'orange';
+});
+
+square.addEventListener('mouseleave', (e) => {
+  square.style.backgroundColor = 'black';
+});
