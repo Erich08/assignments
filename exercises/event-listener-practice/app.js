cc540428d2388470686f7b2a45fa42c9b19e16f0
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-22 21:18:24 -0600

    added event listener for keydown

--- exercises/event-listener-practice/app.js
+++ exercises/event-listener-practice/app.js
@@ -22,6 +22,16 @@ document.body.addEventListener('wheel', (e) => {
   square.style.backgroundColor = 'orange';
 });
 
-square.addEventListener('mouseleave', (e) => {
-  square.style.backgroundColor = 'black';
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'r') {
+    square.style.backgroundColor = 'red';
+  } else if (e.key === 'y') {
+    square.style.backgroundColor = 'yellow';
+  } else if (e.key === 'b') {
+    square.style.backgroundColor = 'blue';
+  } else if (e.key === 'g') {
+    square.style.backgroundColor = 'green';
+  } else if (e.key === 'o') {
+    square.style.backgroundColor = 'orange';
+  }
 });
