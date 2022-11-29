a06e77339f25d38210787f83a05ec5531d57cd90
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-28 21:26:05 -0600

    completed string methods exercise

--- exercises/string-methods-exercise/index.js
+++ exercises/string-methods-exercise/index.js
@@ -1,1 +1,48 @@
 'use strict';
+
+function capitilizeAndLowercase(string) {
+  let uppercase = string.toUpperCase();
+  let lowercase = string.toLowerCase();
+
+  return uppercase + lowercase;
+}
+
+// console.log(capitilizeAndLowercase('random'));
+
+function findMiddleIndex(string) {
+  let middleIndex = Math.floor(string.length / 2);
+
+  return middleIndex;
+}
+
+// console.log(findMiddleIndex('randomstringgoeshere'));
+
+function returnFirstHalf(string) {
+  let findHalf = findMiddleIndex(string);
+  let firstHalf = string.slice(0, findHalf);
+
+  return firstHalf;
+}
+
+// console.log(returnFirstHalf('hello world'));
+
+function capFirstHalfLowSecondHalf(string) {
+  let findHalf = findMiddleIndex(string);
+  let firstHalf = string.slice(0, findHalf).toUpperCase();
+  let secondHalf = string.slice(findHalf).toLowerCase();
+
+  return firstHalf + secondHalf;
+}
+
+// console.log(capFirstHalfLowSecondHalf('hello'));
+
+function capitalize(string) {
+  let arr = string.toLowerCase().split(' ');
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substring(1);
+  }
+
+  return arr.join(' ');
+}
+
+console.log(capitalize('hey friends! practice practice practice!'));
