5036a3ba31106683b18b06ba477ee54a2573e758
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-29 20:52:45 -0600

    Added user options

--- exercises/escape-room/index.js
+++ exercises/escape-room/index.js
@@ -2,10 +2,9 @@
 
 const readLine = require('readline-sync');
 const name = readLine.question('Welcome! What is your name? ');
-// const beginGame = readLine.question(`Hello, ${name} are you ready to begin? `);
-const foundKey = false;
 
-function beginGame() {
+function startGame() {
+  //Forces the user to enter yes because I want them to play my game
   let beginGame;
   do {
     beginGame = readLine
@@ -14,6 +13,19 @@ function beginGame() {
       )
       .toLowerCase();
   } while (beginGame !== 'yes');
-}
 
-beginGame();
+  console.log(
+    'You find yourself in a locked room. You have three options: (1) put your hand in a hole you found in the wall, (2) look for a key, (3) or simply open the door...'
+  );
+
+  //Ensures user enters option 1, 2, or 3.
+  let userOption = readLine.keyIn(
+    'Which option do you choose? Choose wisely...  ',
+    { limit: '$<1-3>' }
+  );
+
+  //   while (userOption !== 1 || userOption !== 2 || userOption !== 3) {
+  //     console.log('Please enter 1, 2, or 3');
+  //   }
+}
+startGame();
