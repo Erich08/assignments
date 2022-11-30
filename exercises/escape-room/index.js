ac276239b019406dc846517bbce33ed28ae8452e
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-11-29 20:03:47 -0600

    basic logic added for getting user name

--- exercises/escape-room/index.js
+++ exercises/escape-room/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const readLine = require('readline-sync');
+const name = readLine.question('Welcome! What is your name? ');
+// const beginGame = readLine.question(`Hello, ${name} are you ready to begin? `);
+const foundKey = false;
+
+function beginGame() {
+  let beginGame;
+  do {
+    beginGame = readLine
+      .question(
+        `Hello, ${name}. Are you ready to begin? Yes or yes? HINT: You don't have a choice because you're locked in a room. `
+      )
+      .toLowerCase();
+  } while (beginGame !== 'yes');
+}
+
+beginGame();
