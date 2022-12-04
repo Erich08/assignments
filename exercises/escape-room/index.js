1393d5bd15d7cf1ba4e41433a619ba412c74353b
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-04 11:49:23 -0600

    added additional functionality to escape room

--- exercises/escape-room/index.js
+++ exercises/escape-room/index.js
@@ -2,6 +2,7 @@
 
 const readLine = require('readline-sync');
 const name = readLine.question('Welcome! What is your name? ');
+let guesses = 3;
 
 function startGame() {
   //Forces the user to enter yes because I want them to play my game
@@ -19,13 +20,42 @@ function startGame() {
   );
 
   //Ensures user enters option 1, 2, or 3.
-  let userOption = readLine.keyIn(
-    'Which option do you choose? Choose wisely...  ',
-    { limit: '$<1-3>' }
+  let userOption;
+  userOption = parseInt(
+    readLine.keyIn('Which option do you choose? Choose wisely...  ', {
+      limit: '$<1-3>',
+    })
   );
 
-  //   while (userOption !== 1 || userOption !== 2 || userOption !== 3) {
-  //     console.log('Please enter 1, 2, or 3');
-  //   }
+  console.log(typeof userOption);
+
+  if (userOption === 1) {
+    console.log('You died... better luck next time.');
+  } else if (userOption === 2) {
+    lookForKey();
+  }
 }
+
+//Logic to cover user selecting option 2.
+function lookForKey() {
+  console.log(
+    'You look on the far side of the room and notice a table with a box on top that is labeled "key." It really cannot be this easy, can it?'
+  );
+
+  //Returns true if Y and false if N
+  const yesOrNo = readLine.keyInYN(
+    'Next to the box you see a button that says "DO NOT PRESS." Do you press it? '
+  );
+
+  if (yesOrNo === true) {
+    readLine.question(
+      'You hear a voice.. it is unclear at first, but then you hear it say, "If you can solve this simple riddle.. the box will be opened. How many months of the year have 28 days? You have 3 chances to answer correctly, or you will be locked in here forever."'
+    );
+  }
+}
+
+function playAgain() {
+  startGame();
+}
+
 startGame();
