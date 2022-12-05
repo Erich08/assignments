2ce3b6967060a81954038ff3793ed0e1ce98f121
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-05 15:33:49 -0600

    completed fizzbuzz assignment

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function fizzBuzz() {
+  let arrOfObj = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arrOfObj.push({ FizzBuzz: `${i}` });
+    } else if (i % 3 === 0) {
+      arrOfObj.push({ Fizz: `${i}` });
+    } else if (i % 5 === 0) {
+      arrOfObj.push({ Buzz: `${i}` });
+    } else {
+      console.log(i);
+    }
+  }
+  return arrOfObj;
+}
+
+console.log(fizzBuzz());
