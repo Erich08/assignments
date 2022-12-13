7cddfc70d57d5eca4bb5dc024328da3045a13df1
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-12 22:26:53 -0600

    completed assignment

--- level-two/assignments-level-two/travel-form/index.js
+++ level-two/assignments-level-two/travel-form/index.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const form = document.travelForm;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const firstName = form.firstName.value;
+  const lastName = form.lastName.value;
+  const age = form.age.value;
+  const location = form.location.value;
+  const dietaryRestrictions = [];
+  form.firstName.value = '';
+  form.lastName.value = '';
+  form.age.value = '';
+  form.gender.value = '';
+
+  for (let i = 0; i < form.dietary.length; i++) {
+    if (form.dietary[i].checked) {
+      dietaryRestrictions.push(' ' + form.dietary[i].value);
+    }
+  }
+
+  alert(`
+    First Name: ${firstName}\n
+    Last Name: ${lastName}\n
+    Age: ${age}\n
+    Gender: ${form.gender.value}\n
+    Destination: ${location}\n
+    Dietary Restrictions: ${dietaryRestrictions}
+  `);
+});
