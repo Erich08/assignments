04f8402a8e2de5e81c79ed8c654be406bac4cbf7
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-13 12:43:36 -0600

    completed mario pest control

--- level-two/assignments-level-two/mario-pest-control/index.js
+++ level-two/assignments-level-two/mario-pest-control/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
 const form = document.pestForm;
+const total = document.getElementById('price');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const goomba = form.goomba.value * 5;
+  const piranha = form.piranha.value * 7;
+  const blooper = form.blooper.value * 10;
+  const koopa = form.koopa.value * 15;
+  const payment = goomba + piranha + blooper + koopa;
+  const span = document.createElement('span');
+  span.textContent = ' ' + payment + ' ' + 'Coins';
+  total.appendChild(span);
+  form.goomba.value = '';
+  form.piranha.value = '';
+  form.blooper.value = '';
+  form.koopa.value = '';
+});
