ddaf641c33846005562675a676de1e01116d2b96
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-19 08:51:44 -0600

    Completed go speed clicker assignment

--- level-two/assignments-level-two/go-speed-clicker/index.js
+++ level-two/assignments-level-two/go-speed-clicker/index.js
@@ -1,37 +1,69 @@
 'use strict';
 
+const heading = document.querySelector('header');
 const btnOne = document.getElementById('btn-one');
 const btnTwo = document.getElementById('btn-two');
 const btnOneContainer = document.getElementById('button-one');
 const btnTwoContainer = document.getElementById('button-two');
+const btnOneTotal = document.getElementById('btn-one-total');
+const btnTwoTotal = document.getElementById('btn-two-total');
+const btnOneTimer = document.getElementById('btn-one-timer');
+const btnTwoTimer = document.getElementById('btn-two-timer');
 let btnOneClicks = 0;
 let btnTwoClicks = 0;
+let timeLeftOne = 9;
+let timeleftTwo = 9;
+let spanOne = document.createElement('span');
+let spanTwo = document.createElement('span');
 
 function btnOneClickCount() {
   btnOneClicks++;
   localStorage.setItem('buttonOne', btnOneClicks);
-  let clicks = localStorage.getItem('buttonOne');
-  console.log(clicks);
-  let timer = setInterval(function () {}, 1000);
-  function stopTimer() {
-    clearInterval(timer);
-    btnOne.removeEventListener('click', btnOneClickCount);
-  }
-  setTimeout(stopTimer, 10000);
 }
 
 function btnTwoClickCount() {
   btnTwoClicks++;
   localStorage.setItem('buttonTwo', btnTwoClicks);
-  let clicks = localStorage.getItem('buttonTwo');
-  console.log(clicks);
-  let timer = setInterval(function () {}, 1000);
+}
+
+window.addEventListener('load', (event) => {
+  btnOneTotal.textContent = localStorage.getItem('buttonOne');
+  let timer = setInterval(function () {
+    spanOne.textContent = ` ${timeLeftOne--}`;
+    btnOneTimer.appendChild(spanOne);
+    if (timeLeftOne === -1) {
+      spanOne.textContent = ` Times up!`;
+      btnOneTimer.appendChild(spanOne);
+    }
+  }, 1000);
   function stopTimer() {
+    let clicks = localStorage.getItem('buttonOne');
     clearInterval(timer);
-    btnTwo.removeEventListener('click', btnTwoClickCount);
+    btnOne.removeEventListener('click', btnOneClickCount);
+    btnOneTotal.textContent = `Total: ${clicks}`;
   }
   setTimeout(stopTimer, 10000);
-}
+  console.log('hello');
+});
+
+window.addEventListener('load', (event) => {
+  btnTwoTotal.textContent = localStorage.getItem('buttonTwo');
+  let timer = setInterval(function () {
+    spanTwo.textContent = ` ${timeleftTwo--}`;
+    btnTwoTimer.appendChild(spanTwo);
+    if (timeleftTwo === -1) {
+      spanTwo.textContent = ` Times up!`;
+      btnTwoTimer.appendChild(spanTwo);
+    }
+  }, 1000);
+  function stopTimer() {
+    let clicks = localStorage.getItem('buttonTwo');
+    clearInterval(timer);
+    btnTwo.removeEventListener('click', btnTwoClickCount);
+    btnTwoTotal.textContent = `Total: ${clicks}`;
+  }
+  setTimeout(stopTimer, 10000);
+});
 
 btnOne.addEventListener('click', btnOneClickCount);
 
