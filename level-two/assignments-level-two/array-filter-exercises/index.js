e58e61862ad9aac4c44114fc9fb3b91fec24932f
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-19 10:03:38 -0600

    completed filter exercises

--- level-two/assignments-level-two/array-filter-exercises/index.js
+++ level-two/assignments-level-two/array-filter-exercises/index.js
@@ -1,1 +1,69 @@
 'use strict';
+
+function fiveAndGreaterOnly(arr) {
+  // your code here
+  return arr.filter((num) => num > 5);
+}
+// test
+console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+
+function evensOnly(arr) {
+  // your code here
+  return arr.filter((num) => num % 2 === 0);
+}
+// test
+console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
+
+function fiveCharactersOrFewerOnly(arr) {
+  // your code here
+  return arr.filter((name) => {
+    if (name.length <= 5) {
+      return name;
+    }
+  });
+}
+// test
+console.log(
+  fiveCharactersOrFewerOnly(['dog', 'wolf', 'by', 'family', 'eaten', 'camping'])
+); // ["by", "dog", "wolf", "eaten"]
+
+function peopleWhoBelongToTheIlluminati(arr) {
+  // your code here
+  return arr.filter((name) => name.member);
+}
+// test
+console.log(
+  peopleWhoBelongToTheIlluminati([
+    { name: 'Angelina Jolie', member: true },
+    { name: 'Eric Jones', member: false },
+    { name: 'Paris Hilton', member: true },
+    { name: 'Kayne West', member: false },
+    { name: 'Bob Ziroll', member: true },
+  ])
+);
+// =>
+//[ { name: 'Angelina Jolie', member: true },
+//  { name: 'Paris Hilton', member: true },
+//  { name: 'Bob Ziroll', member: true } ]
+
+function ofAge(arr) {
+  // your code here
+  return arr.filter((age) => {
+    if (age.age >= 18) {
+      return age;
+    }
+  });
+}
+// test
+console.log(
+  ofAge([
+    { name: 'Angelina Jolie', age: 80 },
+    { name: 'Eric Jones', age: 2 },
+    { name: 'Paris Hilton', age: 5 },
+    { name: 'Kayne West', age: 16 },
+    { name: 'Bob Ziroll', age: 100 },
+  ])
+);
+// =>
+//[ { name: 'Angelina Jolie', age: 80 },
+//  { name: 'Bob Ziroll', age: 100 } ]
