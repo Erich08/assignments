a2d46a6bacf654315c7c94aa676d75fa1ee08587
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-12-19 21:31:00 -0600

    completed assignment

--- level-two/assignments-level-two/catchmeifyoucan/index.js
+++ level-two/assignments-level-two/catchmeifyoucan/index.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function sum(x, y) {
+  //check data types first and throw error
+  try {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw 'You must enter numbers, not strings.';
+    } else {
+      return x + y;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// console.log(sum('4', 7));
+
+let user = { username: 'sam', password: '123abc' };
+function login(username, password) {
+  //check credentials
+  try {
+    if (username.username !== 'sam' || username.password !== '123abc') {
+      throw 'Username or password are incorrect.';
+    } else {
+      return 'You have been logged in.';
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(login(user));
