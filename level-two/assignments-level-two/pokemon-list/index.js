e4b453d49ac8fb5773be60f6c1503ba95cf4a5d5
Erich Hartnauer <erich.hartnauer@gmail.com>
2023-01-01 19:49:32 -0600

    Completed assigment

--- level-two/assignments-level-two/pokemon-list/index.js
+++ level-two/assignments-level-two/pokemon-list/index.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('Hello');
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://api.vschool.io/pokemon', true);
+xhr.send();
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const JSONdata = xhr.responseText;
+    const data = JSON.parse(JSONdata);
+    const resultsArr = data.objects[0].pokemon;
+    showData(resultsArr);
+  }
+};
+
+function showData(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const p = document.createElement('p');
+    p.textContent = arr[i].name;
+    document.body.appendChild(p);
+  }
+}
