7ad2360e0a8f539fbada797a0e9387b1e7af32a7
Erich Hartnauer <erich.hartnauer@gmail.com>
2023-01-02 11:22:56 -0600

    added axios CDN and tested to ensure data is being receieved

--- level-two/assignments-level-two/capstone/index.js
+++ level-two/assignments-level-two/capstone/index.js
@@ -2,6 +2,11 @@
 
 const todoform = document.todoform;
 
+axios
+  .get('https://api.vschool.io/erich8/todo')
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err));
+
 todoform.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log('Hello');
