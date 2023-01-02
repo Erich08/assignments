80e11036972172723f743feffea0a3e31f7e11d8
Erich Hartnauer <erich.hartnauer@gmail.com>
2023-01-02 13:42:40 -0600

    added delete button and delete functionality with axios

--- level-two/assignments-level-two/capstone/index.js
+++ level-two/assignments-level-two/capstone/index.js
@@ -17,10 +17,12 @@ todoform.addEventListener('submit', (e) => {
     title: todoform.title.value,
     description: todoform.description.value,
     imgUrl: todoform.imgUrl.value,
+    price: parseInt(todoform.price.value),
   };
   todoform.title.value = '';
   todoform.description.value = '';
   todoform.imgUrl.value = '';
+  todoform.price.value = '';
 
   axios
     .post('https://api.vschool.io/erich8/todo', newTodo)
@@ -31,17 +33,29 @@ todoform.addEventListener('submit', (e) => {
 //Calls clearList to prevent duplicates then loops through the data to append the information to the DOm
 function listData(data) {
   clearList();
+  console.log(data);
 
   for (let i = 0; i < data.length; i++) {
+    console.log(data[i]._id);
     const div = document.createElement('div');
     const h1 = document.createElement('h1');
     h1.textContent = data[i].title;
+    const span = document.createElement('span');
+    span.textContent = data[i].description;
     const p = document.createElement('p');
-    p.textContent = data[i].description;
+    p.textContent = `Price: $${data[i].price}`;
     const img = document.createElement('img');
     img.src = data[i].imgUrl;
-    div.append(h1, p, img);
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete';
+    div.append(h1, span, p, img, btn);
     document.getElementById('todo-list').appendChild(div);
+    btn.addEventListener('click', (e) => {
+      axios
+        .delete(`https://api.vschool.io/erich8/todo/${data[i]._id}`)
+        .then((res) => location.reload())
+        .catch((err) => console.log(err));
+    });
   }
 }
 
