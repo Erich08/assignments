4c2971616bb6593e92140e33399107b99e26a0d5
Erich Hartnauer <erich.hartnauer@gmail.com>
2023-01-02 12:03:26 -0600

    Added GET and POST functionality via form and appending information to the DOM successfully

--- level-two/assignments-level-two/capstone/index.js
+++ level-two/assignments-level-two/capstone/index.js
@@ -2,12 +2,57 @@
 
 const todoform = document.todoform;
 
-axios
-  .get('https://api.vschool.io/erich8/todo')
-  .then((res) => console.log(res.data))
-  .catch((err) => console.log(err));
+//Performs a get request to the API for the data
+function getData() {
+  axios
+    .get('https://api.vschool.io/erich8/todo')
+    .then((res) => listData(res.data))
+    .catch((err) => console.log(err));
+}
 
+//Performs a POST request to the API and then calls getData and sends that data to listData to render the information to the DOM
 todoform.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('Hello');
+  const newTodo = {
+    title: todoform.title.value,
+    description: todoform.description.value,
+    imgUrl: todoform.imgUrl.value,
+  };
+  todoform.title.value = '';
+  todoform.description.value = '';
+  todoform.imgUrl.value = '';
+
+  axios
+    .post('https://api.vschool.io/erich8/todo', newTodo)
+    .then((res) => getData())
+    .catch((err) => console.log(err));
 });
+
+//Calls clearList to prevent duplicates then loops through the data to append the information to the DOm
+function listData(data) {
+  clearList();
+
+  for (let i = 0; i < data.length; i++) {
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    h1.textContent = data[i].title;
+    const p = document.createElement('p');
+    p.textContent = data[i].description;
+    const img = document.createElement('img');
+    img.src = data[i].imgUrl;
+    div.append(h1, p, img);
+    document.getElementById('todo-list').appendChild(div);
+  }
+}
+
+//Handles new list items being added by clearing the DOM in order to prevent duplicates
+function clearList() {
+  const todolist = document.getElementById('todo-list');
+  while (todolist.firstChild) {
+    todolist.removeChild(todolist.firstChild);
+  }
+}
+
+window.onload = function () {
+  getData();
+};
